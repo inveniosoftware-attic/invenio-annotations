6f091f81cdc1325713ec5082efd94a0812a6ba8b
Yoan Blanc <yoan.blanc@cern.ch>
2014-07-04 22:39:22 +0200

    global: assets 2.0
    * Adds new style bundles to annotations, comments, search and tags.
    
    Signed-off-by: Yoan Blanc <yoan.blanc@cern.ch>

--- bundles.py
+++ bundles.py
@@ -16,3 +16,32 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""Annotations bundles."""
+
+
+from invenio.ext.assets import Bundle
+
+from invenio.modules.previewer.bundles import pdfjs as _pdfjs
+from invenio.modules.comments.bundles import (js as _commentsjs,
+                                              css as _commentscss)
+
+
+_pdfjs.contents += ("js/annotations/pdf_notes_helpers.js",)
+
+_commentsjs.contents += ("js/annotations/notes_popover.js",)
+_commentscss.contents += ("css/annotations/annotations.css",)
+
+
+js = Bundle(
+    "plupload/moxie.js",
+    "plupload/plupload.dev.js",
+    "js/annotations/annotations.js",
+    "js/annotations/plupload_helper.js",
+    filters="uglifyjs",
+    output="gen/annotations.js",
+    name="30-annotations.js",
+    bower={
+        "plupload": "latest"
+    }
+)
